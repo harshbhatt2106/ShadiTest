e6a4f2fc6dec043194b09ab3705d37157f46cea7
Harshbhatt <113496500+harshbhatt2106@users.noreply.github.com>
2026-08-28 15:50:27 +0530

    Register EF Core DbContext

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using ShadiTest.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
+
+// For local/test setup, create the database automatically if it does not exist.
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.EnsureCreated();
+}
 
 if (app.Environment.IsDevelopment())
 {
